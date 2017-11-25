d5be41eb991e4ede2fb4b3259e80b3090e992352
denis@kishinevsky.ca <denis@kishinevsky.ca>
2017-11-25 14:00:04 -0500

    change boolean to YES-NO

--- app/helpers/customer_requests_helper.rb
+++ app/helpers/customer_requests_helper.rb
@@ -1,2 +1,3 @@
 module CustomerRequestsHelper
+    
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+class TrueClass
+    def to_yesno
+      'Yes'
+     end
+ end
+ 
+ class FalseClass
+    def to_yesno
+      'No'
+     end
+ end
